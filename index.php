55cc5ec45da7addab547baac2a8800b45715c9b4
Christian Swan <dazzel@online.de>
2015-09-03 20:04:35 +0200

    make footer sticky and responsive

--- index.php
+++ index.php
@@ -15,33 +15,35 @@
 
 </head>
 <body>
-<section class="name-section">
-	<div class="container">
-		<h2 class="full-name">Christian Swan</h2>
-	</div>
-</section>
-<section class="logo-section">
-	<div class="container">
-		<img src="img/dazzit_logo.png" alt="dazzit_logo">
-	</div>
-</section>
-<section class="contact-section">
-	<div class="container">
-		<div class="row">
-			<div class="three columns">&nbsp;</div>
-			<div class="six columns">
-				<a href="https://plus.google.com/+ChristianSwan_de/about" class="icon icon-google" target="_blank"></a>
-				<a href="https://twitter.com/dazzit_de" class="icon icon-twitter" target="_blank"></a>
-				<a href="mailto:&#099;&#111;&#110;&#116;&#097;&#099;&#116;&#064;&#100;&#097;&#122;&#122;&#105;&#116;&#046;&#100;&#101;"
-				   class="icon icon-email" target="_blank"></a>
-				<a href="https://www.facebook.com/swan.christian" class="icon icon-facebook" target="_blank"></a>
-			</div>
-			<div class="three columns">&nbsp;</div>
+<div class="page-wrap">
+	<section class="name-section">
+		<div class="container">
+			<h2 class="full-name">Christian Swan</h2>
 		</div>
-	</div>
-</section>
+	</section>
+	<section class="logo-section">
+		<div class="container">
+			<img src="img/dazzit_logo.png" alt="dazzit_logo">
+		</div>
+	</section>
+	<section class="contact-section">
+		<div class="container">
+			<div class="row">
+				<div class="three columns">&nbsp;</div>
+				<div class="six columns">
+					<a href="https://plus.google.com/+ChristianSwan_de/about" class="icon icon-google" target="_blank"></a>
+					<a href="https://twitter.com/dazzit_de" class="icon icon-twitter" target="_blank"></a>
+					<a href="mailto:&#099;&#111;&#110;&#116;&#097;&#099;&#116;&#064;&#100;&#097;&#122;&#122;&#105;&#116;&#046;&#100;&#101;"
+					   class="icon icon-email" target="_blank"></a>
+					<a href="https://www.facebook.com/swan.christian" class="icon icon-facebook" target="_blank"></a>
+				</div>
+				<div class="three columns">&nbsp;</div>
+			</div>
+		</div>
+	</section>
+</div>
 <footer class="footer-section">
-	<p>Copyright 2015 &copy; by Christian Swan. All Rights Reserved</p>
+	Copyright <?php echo date('Y'); ?> &copy; by Christian Swan. All Rights Reserved
 </footer>
 </body>
 </html>
