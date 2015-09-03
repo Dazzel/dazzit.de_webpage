ebb95b133b6a55089448b29264b43b0006cba9d2
Christian Swan <dazzel@online.de>
2015-09-03 19:59:52 +0200

    fix contact icon alignment

--- index.php
+++ index.php
@@ -28,21 +28,15 @@
 <section class="contact-section">
 	<div class="container">
 		<div class="row">
-			<div class="four columns">&nbsp;</div>
-			<div class="one column">
+			<div class="three columns">&nbsp;</div>
+			<div class="six columns">
 				<a href="https://plus.google.com/+ChristianSwan_de/about" class="icon icon-google" target="_blank"></a>
-			</div>
-			<div class="one column">
 				<a href="https://twitter.com/dazzit_de" class="icon icon-twitter" target="_blank"></a>
-			</div>
-			<div class="one column">
 				<a href="mailto:&#099;&#111;&#110;&#116;&#097;&#099;&#116;&#064;&#100;&#097;&#122;&#122;&#105;&#116;&#046;&#100;&#101;"
 				   class="icon icon-email" target="_blank"></a>
-			</div>
-			<div class="one column">
 				<a href="https://www.facebook.com/swan.christian" class="icon icon-facebook" target="_blank"></a>
 			</div>
-			<div class="four columns">&nbsp;</div>
+			<div class="three columns">&nbsp;</div>
 		</div>
 	</div>
 </section>
