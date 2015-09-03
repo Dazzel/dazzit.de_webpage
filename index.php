7e590755c2c1814b63c935ea146ddfae9dcb657c
Christian Swan <dazzel@online.de>
2015-09-03 19:48:04 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>dazzIT - Christian Swan</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='https://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/normalize.min.css">
+	<link rel="stylesheet" href="css/skeleton.css">
+	<link rel="stylesheet" href="css/main.css">
+
+</head>
+<body>
+<section class="name-section">
+	<div class="container">
+		<h2 class="full-name">Christian Swan</h2>
+	</div>
+</section>
+<section class="logo-section">
+	<div class="container">
+		<img src="img/dazzit_logo.png" alt="dazzit_logo">
+	</div>
+</section>
+<section class="contact-section">
+	<div class="container">
+		<div class="row">
+			<div class="four columns">&nbsp;</div>
+			<div class="one column">
+				<a href="https://plus.google.com/+ChristianSwan_de/about" class="icon icon-google" target="_blank"></a>
+			</div>
+			<div class="one column">
+				<a href="https://twitter.com/dazzit_de" class="icon icon-twitter" target="_blank"></a>
+			</div>
+			<div class="one column">
+				<a href="mailto:&#099;&#111;&#110;&#116;&#097;&#099;&#116;&#064;&#100;&#097;&#122;&#122;&#105;&#116;&#046;&#100;&#101;"
+				   class="icon icon-email" target="_blank"></a>
+			</div>
+			<div class="one column">
+				<a href="https://www.facebook.com/swan.christian" class="icon icon-facebook" target="_blank"></a>
+			</div>
+			<div class="four columns">&nbsp;</div>
+		</div>
+	</div>
+</section>
+<footer class="footer-section">
+	<p>Copyright 2015 &copy; by Christian Swan. All Rights Reserved</p>
+</footer>
+</body>
+</html>
